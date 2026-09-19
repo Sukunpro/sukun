691bb8e5846c2119699238b79b7be0d8e93310e0
Sukunpro <termuxumut@gmail.com>
2026-09-19 11:07:37 +0300

    Add files via upload

---
+++ RAPOR_r871.md
@@ -0,0 +1,7 @@
+# SÜKÛN r871
+
+- Berhetiyye seyir butonlarında r866/r867/r868/r870 çakışan görsel otoriteleri kaldırıldı.
+- Tek CSS paint authority bırakıldı; buton içine IMG enjekte eden runtime kaldırıldı.
+- Başlat/Duraklat ametist, Önceki/Sonraki safir, Bu ismi baştan zümrüt, Bitir yakut.
+- Oynatıcıyı göster düğmesinin dış dikdörtgen backing katmanı sıfırlandı.
+- Uygulama etiketi, build meta ve SW r871 olarak eşitlendi.
--- sw.js
+++ sw.js
@@ -4,10 +4,10 @@
    kesmez; sayfa reload kararı istemci tarafında verilir. */
 'use strict';
 
-const SURUM = 'r870';
-const CACHE = 'sukun-r870-20260919a';
-const CACHE_META = './__sukun_cache_meta_r870__.json';
-const BUILD_MARKER = './__sukun_build_r870__.json';
+const SURUM = 'r871';
+const CACHE = 'sukun-r871-20260919b';
+const CACHE_META = './__sukun_cache_meta_r871__.json';
+const BUILD_MARKER = './__sukun_build_r871__.json';
 const LATEST_MARKER = './__sukun_latest__.json';
 
 /* Kurulumu kırabilecek büyük/görsel dosyaları zorunlu listeye koymuyoruz.
