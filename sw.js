0f90bf96b991a82dd616309c04a4b56bae2f5f67
Sukunpro <termuxumut@gmail.com>
2026-09-19 10:09:54 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,10 +4,10 @@
    kesmez; sayfa reload kararı istemci tarafında verilir. */
 'use strict';
 
-const SURUM = 'r868';
-const CACHE = 'sukun-r868-20260919a';
-const CACHE_META = './__sukun_cache_meta_r868__.json';
-const BUILD_MARKER = './__sukun_build_r868__.json';
+const SURUM = 'r869';
+const CACHE = 'sukun-r869-20260919a';
+const CACHE_META = './__sukun_cache_meta_r869__.json';
+const BUILD_MARKER = './__sukun_build_r869__.json';
 const LATEST_MARKER = './__sukun_latest__.json';
 
 /* Kurulumu kırabilecek büyük/görsel dosyaları zorunlu listeye koymuyoruz.
